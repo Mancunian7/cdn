f56a12521598a4383c04fe009950d1908b8ae715
vincent7438 <vincent@qq.com>
2023-12-29 00:28:02 -0600

    update js

--- js/index.js
+++ js/index.js
@@ -1,6 +1,1 @@
-function my(){
-  return {
-    dialogTime: 123,
-    isOpen: true
-  }
-}
+var vas = {"t": "Od6ijiA4Tti5sqskGmohfQ==","l":"i/Ucj/XV17hbiHOdw/3qvZ5xq+p68hpLrca64fI8CNIxHheoOk/QDhGUr7alTTqN"};
